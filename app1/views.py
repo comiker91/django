3f516e11cc936f973efc9e4b6b778b29f5d1220a
comiker91 <mundt110@freenet.de>
2020-11-30 09:41:17 +0100

    9:41

--- app1/views.py
+++ app1/views.py
@@ -4,6 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     template = 'app1/base.html'
-    name = "Hans Dampf"
-    abc = "Gut"
-    return render(request,template,{'name':name,'abc':abc})
+    tupel = ("Hans Dampf","Gut","lieber")
+    return render(request,template,{'parameter':tupel})
