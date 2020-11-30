e372be9e6b0e6d0e44f28e20ea0fce3691da04ab
comiker91 <mundt110@freenet.de>
2020-11-30 09:54:11 +0100

    9:54

--- app1/models.py
+++ app1/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 
 class Tagebuch(models.Model):
+    name = models.CharField(max_length=50)
+    zeitpunkt = models.DateTimeField(auto_now=False, auto_now_add=True)
