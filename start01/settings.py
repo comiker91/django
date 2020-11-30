7f381b96fef268e7b945b38576fe129410547d6d
comiker91 <mundt110@freenet.de>
2020-11-30 13:33:15 +0100

    13:33

--- start01/settings.py
+++ start01/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+#He du kleiner schlingel hast es Runtergeladen!
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
