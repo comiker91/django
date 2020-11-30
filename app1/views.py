56cf70c9ff0d40ea47575a411d9b67844ebeda48
comiker91 <mundt110@freenet.de>
2020-11-30 10:52:44 +0100

    10:52

--- app1/views.py
+++ app1/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
 # Create your views here.
 def index(request):
     template = 'app1/base.html'
     tupel = ("Hans Dampf","Gut","lieber")
-    return render(request,template,{'parameter':tupel})
+    tabelle = Tagebuch.objects.all()
+    return render(request,template,{'parameter':tupel, 'tabelle': tabelle})
