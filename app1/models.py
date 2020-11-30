e229b6414a4a68332a89073561a585dc03ebfa16
comiker91 <mundt110@freenet.de>
2020-11-30 09:55:23 +0100

    9:55

--- app1/models.py
+++ app1/models.py
@@ -6,3 +6,4 @@ class Tagebuch(models.Model):
     name = models.CharField(max_length=50)
     zeitpunkt = models.DateTimeField(auto_now=False, auto_now_add=True)
     kommentar = models.TextField()
+    bewertung = models.IntegerField()
