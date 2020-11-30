a90598b53f6f05d0787fa05febcbe2cf2c76fcfe
comiker91 <mundt110@freenet.de>
2020-11-30 09:54:47 +0100

    9:55

--- app1/models.py
+++ app1/models.py
@@ -5,3 +5,4 @@ from django.db import models
 class Tagebuch(models.Model):
     name = models.CharField(max_length=50)
     zeitpunkt = models.DateTimeField(auto_now=False, auto_now_add=True)
+    kommentar = models.TextField()
