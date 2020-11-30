aecb4a7d1769e2772790013fb199f006fb22ae95
comiker91 <mundt110@freenet.de>
2020-11-30 10:59:37 +0100

    11:00

--- app1/views.py
+++ app1/views.py
@@ -7,5 +7,4 @@ def index(request):
     template = 'app1/base.html'
     tupel = ("Hans Dampf","Gut","lieber")
     tabelle = Tagebuch.objects.all()
-    print(tabelle)
     return render(request,template,{'parameter':tupel, 'tabelle': tabelle})
