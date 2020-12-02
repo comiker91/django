b0d4af316243550a415e662ba669ef7eeb62f282
comiker91 <mundt110@freenet.de>
2020-12-02 08:10:05 +0100

    8:10

--- app1/urls.py
+++ app1/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('tagebuchdetail/<int:comment_id>', views.tgbDetail, name='index'),
+    path('tagebuchdetail/<int:comment_id>', views.tgbDetail, name='tgbdetails'),
 ]
